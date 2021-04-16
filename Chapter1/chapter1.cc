9d6f026bfb3a8b463956e95b527c0e42f2672472
Renzo WANG <wrzeternal@163.com>
2021-04-16 14:44:19 +0800

    Chapter1.2 practic

--- Chapter1/chapter1.cc
+++ Chapter1/chapter1.cc
@@ -4,6 +4,9 @@ int main()
     std::cout << "enter 2 numbers:" <<std::endl;
     int v1 = 0, v2 = 0;
     std::cin >> v1 >> v2;
-    std::cout <<" the sum of " << v1 <<"and"<< v2 << "is" << v1+v2 <<std::endl;
+    std::cout << "Hello, world." <<std::endl;
+    std::cout <<" the sum of " << v1 <<"and"<< v2 << " is " << v1+v2 <<std::endl;
+    std::cout <<" The product of " << v1 <<" and " <<v2 <<" is " << v1*v2 <<std::endl;
+    
     return 0;
 }
